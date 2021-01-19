c14950791341ca8b0dd01cc9d557b4917b53af51
woobuntu <boutime2017@gmail.com>
2021-01-19 15:48:54 +0900

    Mobx를 배우자

---
+++ README.md
@@ -0,0 +1,3 @@
+Mobx
+
+Mobx는 state를 observable이 감싸주면, 해당 observable이 action을 감지할 때마다 observer에 통지해주는 방식으로 동작한다.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p>mobx를 배우자</p>
+    </div>
+  );
+}
+
+export default App;
